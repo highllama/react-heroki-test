60ccd45425ddfb980a2843cc7614ce3678891d7b
Juan Diego Rodriguez <juanaviator@hotmail.com>
2020-07-18 18:16:17 -0500

    first test

---
+++ precache-manifest.605e642ff45c1eff476f2f6cbed7c574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8e02db98eade9f0f7d8da17e40a2a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "135e5d7aa71b1f72af9f",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c56f035e639dcf97e74b",
+    "url": "/static/css/main.52c4787c.chunk.css"
+  },
+  {
+    "revision": "135e5d7aa71b1f72af9f",
+    "url": "/static/js/2.c879c646.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c879c646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56f035e639dcf97e74b",
+    "url": "/static/js/main.13c3e8de.chunk.js"
+  },
+  {
+    "revision": "9a00744d497bca1272d1",
+    "url": "/static/js/runtime-main.ec017363.js"
+  }
+]);
